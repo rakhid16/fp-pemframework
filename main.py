2bef8da0f5a1e4ad6c729443756b706fc42bd677
rakhid16 <roploverz@gmail.com>
2020-04-03 21:36:25 +0700

    semua link sudah nyambung

--- main.py
+++ main.py
@@ -12,12 +12,52 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-  return render_template("dashboard.html")
+  return render_template("dashboard/dashboard.html")
 
 @app.route('/login')
 def login():
-  return render_template("login.html")
+  return render_template("auth/login.html")
 
 @app.route('/forgot_pwd')
 def forgot_pwd():
-  return render_template("forgot-password.html")
+  return render_template("auth/forgot-password.html")
+
+@app.route('/404')
+def not_found():
+  return render_template("dashboard/404.html")
+
+@app.route('/blank')
+def blank():
+  return render_template("dashboard/blank.html")
+
+@app.route('/buttons')
+def buttons():
+  return render_template("dashboard/buttons.html")
+
+@app.route('/cards')
+def cards():
+  return render_template("dashboard/cards.html")
+
+@app.route('/charts')
+def charts():
+  return render_template("dashboard/charts.html")
+
+@app.route('/tables')
+def tables():
+  return render_template("dashboard/tables.html")
+
+@app.route('/utilities_a')
+def utilities_a():
+  return render_template("dashboard/utilities-animation.html")
+
+@app.route('/utilities_b')
+def utilities_b():
+  return render_template("dashboard/utilities-border.html")
+
+@app.route('/utilities_c')
+def utilities_c():
+  return render_template("dashboard/utilities-color.html")
+
+@app.route('/utilities_o')
+def utilities_o():
+  return render_template("dashboard/utilities-other.html")
